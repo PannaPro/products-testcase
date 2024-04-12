a568e477cd62667c93abf7324933b46f39f4ccdb
PannaPro <pannacaliano@gmail.com>
2024-04-12 17:31:22 +0400

    Refactoring

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,10 +14,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         
-        // $user = User::factory()->create();
-        $this->call(CreateSuperUserSeeder::class);
-        $this->call(ProductSeeder::class);
+        $this->call([
+            CreateSuperUserSeeder::class,
+            ProductSeeder::class,
+            PermissionsSeeder::class,
+        ]);
         
+        User::factory(5)->create();
+
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
---
+++ tests/Unit/ProductControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\Product;
+use PHPUnit\Framework\TestCase;
+
+
+class ProductControllerTest extends TestCase
+{
+    /** @test */
+    
+    
+}    
+
